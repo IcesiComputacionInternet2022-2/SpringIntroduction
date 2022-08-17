5a6d69fa14e7d7217563cf79fe27b8fd122705d0
Ricardo D. Cuero <47835668+merolemay@users.noreply.github.com>
2022-08-16 23:02:34 -0500

    Almost forgot to summit this b4 sleeping kekw

---
+++ src/main/java/com/icesi/demo/service/AnagramService.java
@@ -0,0 +1,13 @@
+package com.icesi.demo.service;
+
+import com.icesi.demo.dto.AnagramDTO;
+import com.icesi.demo.dto.ConcatenatedResponseDTO;
+
+import java.util.List;
+
+public interface AnagramService {
+
+
+    public AnagramDTO anagramTwoWords(String a1,String a2);
+
+}
--- src/main/java/com/icesi/demo/service/StringListService.java
+++ src/main/java/com/icesi/demo/service/StringListService.java
@@ -1,14 +1,12 @@
 package com.icesi.demo.service;
 
 import com.icesi.demo.dto.ConcatenatedResponseDTO;
-import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 public interface StringListService {
 
 
-    public ConcatenatedResponseDTO concatenateStrings(List<String> stringList);
-
+    public  ConcatenatedResponseDTO concatenateStrings(List<String> stringList);
 
 }
