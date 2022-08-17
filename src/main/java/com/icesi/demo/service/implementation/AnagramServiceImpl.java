bd05d36a8554e2cac0ef35e73aabc553c306ba5f
BrianR <briomero76@gmail.com>
2022-08-17 14:48:59 -0500

    minor changes on anagram service

--- src/main/java/com/icesi/demo/service/implementation/AnagramServiceImpl.java
+++ src/main/java/com/icesi/demo/service/implementation/AnagramServiceImpl.java
@@ -15,11 +15,13 @@ public class AnagramServiceImpl implements AnagramService{
         boolean response = false;
         if(stringList.size() == 2){
             if(stringList.get(0) != null && stringList.get(1) != null && stringList.get(0).length() == stringList.get(1).length()){
-                char[] f = stringList.get(0).toCharArray();
-                Arrays.sort(f);
-                char[] s = stringList.get(1).toCharArray();
-                Arrays.sort(s);
-                response = String.valueOf(f).equalsIgnoreCase(String.valueOf(s));
+                if(!stringList.get(0).matches(".*\\d.*") && !stringList.get(1).matches(".*\\d.*")){
+                    char[] f = stringList.get(0).toCharArray();
+                    Arrays.sort(f);
+                    char[] s = stringList.get(1).toCharArray();
+                    Arrays.sort(s);
+                    response = String.valueOf(f).equalsIgnoreCase(String.valueOf(s));
+                }
             }
         }//End if
         return new AnagramResponseDTO(response);
