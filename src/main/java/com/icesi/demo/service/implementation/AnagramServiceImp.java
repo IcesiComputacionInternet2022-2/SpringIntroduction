3885548199da52106060701ef719051b2b539bc5
JulianAndresRiveraCarrillo <julianandresrivera29@hotmail.com>
2022-08-17 14:09:45 -0500

    Code changed

--- src/main/java/com/icesi/demo/service/implementation/AnagramServiceImp.java
+++ src/main/java/com/icesi/demo/service/implementation/AnagramServiceImp.java
@@ -12,13 +12,27 @@ public class AnagramServiceImp implements AnagramService {
 
     @Override
     public AnagramResponseDTO anagramString(List<String> words){
-        String cad1 = words.get(0).toUpperCase();
-        String cad2 = words.get(1).toUpperCase();
+        String cad1 = words.get(0);
+        String cad2 = words.get(1);
 
-        cad1 = sort(cad1);
-        cad2 = sort(cad2);
+        boolean c1 = cad1.matches("[+-]?\\d*(\\.\\d+)?");
+        boolean c2 = cad2.matches("[+-]?\\d*(\\.\\d+)?");
 
-        return new AnagramResponseDTO(cad1.equals(cad2));
+        System.out.println(c1);
+        System.out.println(c2);
+
+
+        AnagramResponseDTO responseDTO = new AnagramResponseDTO(false);
+
+        if(!c1 && !c2){
+            cad1 = sort(cad1.toUpperCase());
+            cad2 = sort(cad2.toUpperCase());
+            responseDTO.setResult(cad1.equals(cad2));
+            System.out.println(responseDTO.isResult());
+
+        }
+
+        return responseDTO;
     }
 
     private String sort(String cadena) {
