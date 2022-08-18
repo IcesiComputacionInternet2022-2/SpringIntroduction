f0a286df93b7dd219c9b5c0aec52795df32139ef
Juan David Cruz Garcia <angrybirdsgallo@gmail.com>
2022-08-18 15:41:51 -0500

    Test Added

---
+++ src/test/java/com/icesi/demo/service/AnagramServiceTest.java
@@ -0,0 +1,90 @@
+package com.icesi.demo.service;
+
+import com.icesi.demo.dto.ConfirmationDTO;
+import com.icesi.demo.service.implementation.AnagramServiceImpl;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class AnagramServiceTest {
+
+    private AnagramService anagramService;
+
+    @BeforeEach
+    private void init(){
+        anagramService= new AnagramServiceImpl();
+    }
+
+    @Test
+    public void testIsAnagram(){
+        List<String> stringList = new ArrayList<>();
+        stringList.add("listen");
+        stringList.add("silent");
+
+        ConfirmationDTO result = anagramService.checkAnagrams(stringList);
+
+        assertTrue(result.isAnagram());
+    }
+
+    @Test
+    public void testAnagramWordsLengthIsDifferent(){
+        List<String> stringList = new ArrayList<>();
+        stringList.add("a");
+        stringList.add("ab");
+
+        ConfirmationDTO result = anagramService.checkAnagrams(stringList);
+
+        assertFalse(result.isAnagram());
+    }
+
+    @Test
+    public void testAnagramCaseInsensitive(){
+        List<String> stringList = new ArrayList<>();
+        stringList.add("Listen");
+        stringList.add("Silent");
+
+        ConfirmationDTO result = anagramService.checkAnagrams(stringList);
+
+        assertTrue(result.isAnagram());
+    }
+
+    @Test
+    public void testNotAnagram(){
+        List<String> stringList = new ArrayList<>();
+        stringList.add("String");
+        stringList.add("Strong");
+
+        ConfirmationDTO result = anagramService.checkAnagrams(stringList);
+
+        assertFalse(result.isAnagram());
+    }
+
+    @Test
+    public void testAnagramRepeatedLetters(){
+        List<String> stringList = new ArrayList<>();
+        stringList.add("listen");
+        stringList.add("sillent");
+
+        ConfirmationDTO result = anagramService.checkAnagrams(stringList);
+
+        assertFalse(result.isAnagram());
+    }
+
+    @Test
+    public void testWordOneIsLongerThanTheSecond(){
+        List<String> stringList = new ArrayList<>();
+        stringList.add("llisten");
+        stringList.add("Silent");
+
+        ConfirmationDTO result = anagramService.checkAnagrams(stringList);
+
+        assertTrue(result.isAnagram());
+    }
+
+    
+}
